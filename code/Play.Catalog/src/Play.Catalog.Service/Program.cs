68769a7ab841dbdd944c477bfb141537e9f6d0a9
randomspaghettiguy <thinh.phamcea@gmail.com>
2024-07-17 22:04:43 +0700

    repository pattern with mongodb

--- code/Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ code/Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,8 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ code/Play.Catalog/src/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -0,0 +1,50 @@
+using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+
+namespace Play.Catalog.Service.Repositories
+{
+    public class ItemsRepository
+    {
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> dbCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        public ItemsRepository()
+        {
+            var mongoClient = new MongoClient("mongodb://localhost:27017/");
+            var database = mongoClient.GetDatabase("Catalog");
+            dbCollection = database.GetCollection<Item>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<Item>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<Item> GetAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(existingEntity => existingEntity.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Item entity)
+        {
+            var toCreate = entity ?? throw new ArgumentNullException(nameof(entity));
+            await dbCollection.InsertOneAsync(toCreate);
+        }
+
+        public async Task UpdateAsync(Item entity)
+        {
+            var toCreate = entity ?? throw new ArgumentNullException(nameof(entity));
+
+            FilterDefinition<Item> filter = filterBuilder.Eq(existingEntity => existingEntity.Id, toCreate.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(existingEntity => existingEntity.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
+        }
+    }
+}
